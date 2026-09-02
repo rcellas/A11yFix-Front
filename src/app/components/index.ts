6747ef96b86f42893facf4cdf59fc81a19b42ed3
rcellas <rocio.cejudo.soronellas@gmail.com>
2026-09-02 23:45:16 +0200

    feat(components): export components from barrel index

---
+++ src/app/components/index.ts
@@ -0,0 +1,6 @@
+export * from './button/button.component';
+export * from './badge/badge.component';
+export * from './card/card.component';
+export * from './text-field/text-field.component';
+export * from './filter-chip-group/filter-chip-group.component';
+export * from './code-diff-viewer/code-diff-viewer.component';
